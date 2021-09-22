307fb6e9e6c2600e3d3b6c35f055a59e8fe89b77
batonXIII <gleb.big03@gmail.com>
2021-09-22 23:39:10 +0300

    1

---
+++ nomer5d.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+n =2
+b =(n+n**2+n**3+n**4+n**5)
+print(b)
---
+++ nomer7d.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+n = 5
+if n % 2 == 0:
+    print('Четное')
+else:
+        print('Нечетное')
