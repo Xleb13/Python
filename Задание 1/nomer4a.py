050453e88a50da15025fb40e0a69a78b7f6bdbd2
batonXIII <gleb.big03@gmail.com>
2021-10-09 15:42:17 +0300

    1

--- Задание 1/nomer4a.py
+++ Задание 1/nomer4a.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-sec = int(input("Введите количество секунд: "))
-day = int((sec/86400))
-hours = int(((sec % 86400)/3600))
-minut = int(((sec % 86400) % 3600) / 60)
-seconds = int(((sec % 86400) % 3600) % 60)
-print('Дней: '+str(day))
-print('Часов: '+str(hours))
-print('Минут: '+str(minut))
-print('Секунд: '+str(seconds))
+
+# Четвёртое задание
+def lengthOfLace():
+    print("Введите входные данные (4 числа)")
+    a = int(input("Расстояние между рядами - "))
+    b = int(input("Расстояние между отверстиями в ряду - "))
+    l = int(input("Длина свободного конца шнурка - "))
+    N = int(input("Кол-во отверстий в ряду - "))
+    print("Ответ:")
+    return (2 * N - 1) * a + 2 * l + 2 * (N - 1) * b
+
+print("Четвёртое задание: ")
+print(lengthOfLace())
+print("\n")
